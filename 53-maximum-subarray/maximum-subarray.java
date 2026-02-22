42e297cb1573acbffcf581be0d6e1750c1a6a039
Sathish10-code <sathish.26csb@licet.ac.in>
2026-02-22 17:15:02 +0530

    Time: 1 ms (99.69%) | Memory: 77.4 MB (47.46%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max  = nums[0];
+        int currsum=0;
+        for(int n:nums){
+            currsum+=n;
+            if(currsum>max){
+                max=Math.max(currsum,max);
+            }
+            if(currsum<0){
+                currsum=0;
+            }
+        }
+        return max;
+    }
+}
