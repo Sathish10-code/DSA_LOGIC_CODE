ff3c104ddc64dbbbfa7765e1e5c90dfe78667ace
Sathish10-code <sathish.26csb@licet.ac.in>
2026-04-05 07:21:00 +0530

    Time: 3 ms (23.48%) | Memory: 19.2 MB (95.09%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        count=0
+        i=0
+        j=0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                count+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        
+        return len(s)==count
