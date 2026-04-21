3e2fc75321315f87bac00f7bf0db8a360d025f62
Sathish10-code <sathish.26csb@licet.ac.in>
2026-04-21 11:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (57.34%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        odd = [0]*len(graph)
+
+        def dfs(i):
+            if odd[i]:
+                return True
+            q = deque([i])
+            odd[i]=-1
+            while q:
+                i = q.popleft()
+                for nei in graph[i]:
+                    if odd[nei]==odd[i]:
+                        return False
+                    elif not odd[nei]:
+                        q.append(nei)
+                        odd[nei] = -1 * odd[i]
+            return True
+        
+        for i in range(len(graph)):
+            if not dfs(i):
+                return False
+        return True
