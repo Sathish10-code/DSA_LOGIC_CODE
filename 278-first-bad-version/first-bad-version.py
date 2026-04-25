336ea236d19239c6fee932a2f5561bd320e609f7
Sathish10-code <sathish.26csb@licet.ac.in>
2026-04-25 07:36:10 +0530

    Time: 42 ms (72.79%) | Memory: 19.2 MB (67.46%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -4,10 +4,10 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         l,r = 1,n
-        while l<r:
+        while l<=r:
             m = (l+r)//2
             if isBadVersion(m):
-                r=m
+                r=m-1
             else:
                 l=m+1
         return l
