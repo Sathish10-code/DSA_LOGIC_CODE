0820d675750b2d25fe65b2ba85943f8197bb93b8
Sathish10-code <sathish.26csb@licet.ac.in>
2026-05-12 07:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,13 +1,10 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nge_map ={}
+        nge_map={}
         stack=[]
-
         for n in nums2:
             while stack and stack[-1]<n:
-                num = stack.pop()
+                num=stack.pop()
                 nge_map[num]=n
             stack.append(n)
-        
-
         return [nge_map.get(n,-1) for n in nums1]
