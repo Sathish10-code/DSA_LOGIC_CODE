d54159dac01727f715c7e0be078d8567ac37e19c
Sathish10-code <sathish.26csb@licet.ac.in>
2026-05-15 17:20:50 +0530

    Time: 32 ms (78.10%) | Memory: 37.6 MB (72.86%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,9 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        rank_map={}
-        rank=1
-        for num in sorted(arr):
-            if num not in rank_map:
-                rank_map[num]=rank
-                rank+=1
+        # rank_map={}
+        # rank=1
+        # for num in sorted(arr):
+        #     if num not in rank_map:
+        #         rank_map[num]=rank
+        #         rank+=1
+        # return [rank_map[num] for num in arr]
+        sorted_uni_arr = sorted(list(set(arr)))
+        rank_map = {val:i+1 for i,val in enumerate(sorted_uni_arr)}
         return [rank_map[num] for num in arr]
