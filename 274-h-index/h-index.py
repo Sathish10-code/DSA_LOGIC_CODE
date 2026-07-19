af94957a6f70595e270dd61e75ca4e8c6cd1b283
Sathish10-code <sathish.26csb@licet.ac.in>
2026-07-19 17:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (91.75%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        for idx , cit in enumerate(citations):
+            if idx >=cit:
+                return idx
+        return len(citations)
