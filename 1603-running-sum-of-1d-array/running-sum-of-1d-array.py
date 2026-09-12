a1639a0a6ca5d102ad250a060bff4061cf93e658
Sathish10-code <sathish73811@gmail.com>
2026-09-12 13:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.38%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if i==0:
-                continue
+        for i in range(1,len(nums)):
+          
             nums[i]+=nums[i-1]
         return nums
